5853f64ce7210601c8bda53daae14d04da4e2a77
Francisco Pontes <fpp-dev@pop-os.localdomain>
2021-02-17 19:07:10 +0000

    Url sorting and navbar sorting

--- web_app/pages/urls.py
+++ web_app/pages/urls.py
@@ -4,9 +4,9 @@ from .views import home_view, about_view, contact_view, projects_view
 
 app_name = "pages"
 urlpatterns = [
-    path('home/', home_view, name='home-view'),
+    path('home/', home_view, name='home'),
     path('', home_view),
-    path('about/', about_view, name='about-view'),
-    path('contact/', contact_view, name='contact-view'),
-    path('projects/', projects_view, name='projects-view'),
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
+    path('projects/', projects_view, name='projects'),
 ]
