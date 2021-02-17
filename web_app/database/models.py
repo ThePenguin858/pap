da0f4e5c2cb77f20de6889a325dca062961b18af
Francisco Pontes <fpp-dev@pop-os.localdomain>
2021-02-17 22:04:14 +0000

    Adding all of checkout and checkout rendering.

--- web_app/database/models.py
+++ web_app/database/models.py
@@ -40,6 +40,18 @@ class Order(models.Model):
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
     def __str__(self):
         return str(self.id)
 
@@ -48,6 +60,10 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=9, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        return self.product.price * self.quantity
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
--- web_app/database/views.py
+++ web_app/database/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 # from .forms import EditClientForm, ClientForm, InvoiceForm
 from pages.views import home_view
-from .models import Product
+from .models import *
 # Create your views here.
 
 
@@ -10,109 +10,43 @@ def store_view(request):
     """ List all of the invoices """
     queryset = Product.objects.all()
 
+    if request.method == 'POST':
+        OrderItem.objects.append({'product': product, 'order': order,'quantity': 1})
+
     context = {
         'products': queryset
     }
     return render(request, 'database/store.html', context)
 
 def cart_view(request):
-    context = {}
+    queryset = Product.objects.all()
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {
+        'items': items,
+        'order': order
+    }
+
     return render(request, 'database/cart.html', context)
 
 def checkout_view(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {
+        'items': items,
+        'order': order
+    }
     return render(request, 'database/checkout.html', context)
-
-# def list_clients_view(request):
-#     """ List all of the clients """
-#     queryset = Client.objects.all()
-
-#     context = {
-#         'object_list': queryset
-#     }
-
-#     return render(request, 'database/list_views/list_client.html', context)
-
-
-# def create_invoice_view(request, *args, **kwargs):
-#     """ View for creating receipts urls=create_invoice """
-
-#     my_form = InvoiceForm(request.POST or None)
-
-#     if request.method == 'POST':
-#         if my_form.is_valid():
-#             my_form.save()
-#             my_form = InvoiceForm()
-
-#     context = {'form': my_form}
-
-#     return render(request, 'database/forms/invoice_create.html', context)
-
-
-# def create_client_view(request, *args, **kwargs):
-#     """ View for creating client urls=create """
-
-#     new_form = ClientForm(request.POST or None)
-
-#     if request.method == 'POST':
-#         if new_form.is_valid():
-#             new_form.save()
-#             new_form = ClientForm()
-
-#     context = {'Form': new_form}
-
-#     return render(request, 'database/forms/client_create.html', context)
-
-
-# def client_update_view(request, my_name):
-#     obj = Client.objects.get(name=my_name)
-#     form = EditClientForm(instance=obj)
-#     if request.method == 'POST':
-#         form = EditClientForm(request.POST, instance=obj)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('/database/list_clients')
-
-#     context = {
-#         'object': form
-#     }
-#     return render(request, 'database/forms/client_view.html', context)
-
-
-# def invoice_update_view(request, fid):
-#     obj = Invoice.objects.get(fid=fid)
-#     form = InvoiceForm(instance=obj)
-#     if request.method == 'POST':
-#         form = InvoiceForm(request.POST, instance=obj)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('/database/list_invoices')
-
-#     context = {
-#         'object': form
-#     }
-#     return render(request, 'database/forms/invoice_view.html', context)
-
-
-# def delete_invoice_view(request, my_fid):
-#     obj = Invoice.objects.get(fid=my_fid)
-
-#     if request.method == "POST":
-#         obj.delete()
-#         return redirect('database:list-invoices')
-
-#     context = {
-#         'object': obj,
-#     }
-#     return render(request, 'database/forms/delete_invoice.html', context)
-
-
-# def delete_client_view(request, my_name):
-#     obj = Client.objects.get(name=my_name)
-
-#     if request.method == 'POST':
-#         obj.delete()
-#         return redirect('/database/list_clients')
-#     context = {'object': obj, }
-
-#     return render(request, 'database/forms/delete_client.html', context)
